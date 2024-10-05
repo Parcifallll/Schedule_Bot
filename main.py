283a92f9539e82ccd8ef262a81644b93580712d0
Roman <89307076692roman@gmail.com>
2024-10-05 18:26:46 +0300

    init main

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from app.handlers import router
+
+
+async def main():
+    bot = Bot(token="7111650915:AAGDyKfX-0NvziCz33aF-YZyXsln300Kdyo")
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот оффнут")
